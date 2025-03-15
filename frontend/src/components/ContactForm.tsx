e8cbb8dc21e912d00d98c1205847615579b36e8c
TwentyCent <vincent.simonin@hotmail.com>
2025-03-15 11:15:46 +0100

    Update ContactForm.tsx
    Set the correct variable

--- frontend/src/components/ContactForm.tsx
+++ frontend/src/components/ContactForm.tsx
@@ -61,7 +61,7 @@ const ContactForm = ({}: ContactFormProps) => {
 
     const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!
     const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!
-    const userID = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY! + 1
+    const userID = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
 
     try {
       const emailParams = {
